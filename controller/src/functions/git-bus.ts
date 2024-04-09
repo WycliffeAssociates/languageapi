179cf49e6d6f8cf71fa8c8804ce70def63198d7a
Will Kelly <willkelly9617@gmail.com>
2024-04-09 12:24:38 -0500

    fix rebase typos

--- controller/src/functions/git-bus.ts
+++ controller/src/functions/git-bus.ts
@@ -1,8 +1,6 @@
 import {app, InvocationContext} from "@azure/functions";
 import {getDb as startDb} from "../db/config";
 import {z} from "zod";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import {eq, and} from "drizzle-orm";
 import {handlePost as handleGitPost} from "../routes/git";
 import {handlePost as handleContentPost} from "../routes/content";
@@ -13,24 +11,6 @@ import * as dbSchema from "../db/schema/schema";
 const db = startDb();
 
 // import {createId} from "@paralleldrive/cuid2";
-=======
-=======
-import {eq, and} from "drizzle-orm";
->>>>>>> 0d6d1a1 (lang api adjustments)
-import {handlePost as handleGitPost} from "../routes/git";
-import {handlePost as handleContentPost} from "../routes/content";
-import * as validators from "../routes/validation";
-import {checkContentExists} from "../functions/renderings-bus";
-<<<<<<< HEAD
-startDb();
->>>>>>> 90cec3e (add renderings table.  Move gateway to walangmeta. Rename some properties)
-=======
-import {createId} from "@paralleldrive/cuid2";
-import * as dbSchema from "../db/schema/schema";
-const db = startDb();
-
-// import {createId} from "@paralleldrive/cuid2";
->>>>>>> 0d6d1a1 (lang api adjustments)
 
 const latestCommitSchema = z.object({
   Hash: z.string(),
@@ -57,8 +37,6 @@ export async function wacsSbLangApi(
   message: unknown,
   context: InvocationContext
 ) {
-<<<<<<< HEAD
-<<<<<<< HEAD
   const namespace = "wacs";
   // If zod or the db action below throws here, the message will end up in the dead letter queue.
   try {
@@ -123,79 +101,6 @@ export async function wacsSbLangApi(
     const shapedForDb: z.infer<typeof validators.gitPost> = [
       {
         contentId: contentCuid,
-=======
-=======
-  const namespace = "wacs";
->>>>>>> 0d6d1a1 (lang api adjustments)
-  // If zod or the db action below throws here, the message will end up in the dead letter queue.
-  try {
-    context.log(message);
-    let contentCuid = null; //we need the guid of a content row to insert or upsert on the git Table.
-    const parsed = eventSchema.parse(message);
-    context.log(
-      `GIT BUS RECEIVED: received a message for ${parsed.Repo} of event type ${parsed.EventType}`
-    );
-    // todo: check on name/namesapce,
-    const {exists, id} = await checkContentExists({
-      name: `${parsed.User}/${parsed.Repo}`.toLowerCase(),
-      namespace: namespace,
-    });
-    if (!exists) {
-      context.log(
-        `namespace: ${namespace} and name: ${parsed.User}/${parsed.Repo} is not already in api. Creating new row in table`
-      );
-      const cuid = createId();
-      const newContentPayload = {
-        id: cuid,
-        name: `${parsed.User}/${parsed.Repo}`.toLowerCase(),
-        namespace: namespace,
-        type: "text",
-      } as const;
-      const newContentRow: z.infer<typeof validators.contentPost> = [
-        newContentPayload,
-      ];
-      const newRowRes = await handleContentPost(newContentRow);
-      if (newRowRes.status !== 200) {
-        contentCuid = cuid;
-        context.log(
-          `Failed to create new content row for ${`${parsed.User}/${parsed.Repo}`.toLowerCase()}`
-        );
-        throw new Error(
-          `Failed to create new content row for ${`${parsed.User}/${parsed.Repo}`.toLowerCase()}`
-        );
-      }
-    }
-    contentCuid = id ? id : contentCuid;
-    if (!contentCuid) {
-      let matchingRow = await db
-        .select()
-        .from(dbSchema.content)
-        .where(
-          and(
-            eq(
-              dbSchema.content.name,
-              `${parsed.User}/${parsed.Repo}`.toLowerCase()
-            ),
-            eq(dbSchema.content.namespace, namespace)
-          )
-        );
-      if (!matchingRow[0]) {
-        throw new Error(
-          `Could not find content row for ${`${parsed.User}/${parsed.Repo}`.toLowerCase()}`
-        );
-      }
-      contentCuid = matchingRow[0].id;
-    }
-    // api built with bulk ops in mind, so arrays are passed, even for single op, versus having insertSingle vs insertMany type routes
-    const shapedForDb: z.infer<typeof validators.gitPost> = [
-      {
-<<<<<<< HEAD
-        namespace: "wacs",
-        contentId: `${parsed.User}/${parsed.Repo}`,
->>>>>>> 90cec3e (add renderings table.  Move gateway to walangmeta. Rename some properties)
-=======
-        contentId: contentCuid,
->>>>>>> 0d6d1a1 (lang api adjustments)
         repoName: parsed.Repo,
         repoUrl: parsed.RepoHtmlUrl,
         username: parsed.User,
